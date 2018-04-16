2eb6195b3e9b311e151f96bb5d3f303fd375ec02
Michele Dexter <micheleannedexter@gmail.com>
2018-04-16 11:25:14 -0500

    delete employee - bonus it totally updates the monthly cost too

--- README.md
+++ README.md
@@ -9,9 +9,9 @@ Create an application that records employee salaries and adds salaries up to rep
 - [X] Show all the emplyees on the DOM
 - [X] Calculate the monthly salary cost (show on DOM)
 - [X] Add the employee to a table
-- [ ] If monthly cost was above limit $20,000, change color to red
-- [ ] Add a delete button to remove employee
+- [X] If monthly cost was above limit $20,000, change color to red
+- [X] Add a delete button to remove employee
 - [ ] Pretty up the styling
-- [ ] Create an Employee class
-- [ ] Update monthly salary total when employee is deleted
+- [X] Create an Employee class
+- [X] Update monthly salary total when employee is deleted
 - [ ] Change "form" to a real form
--- script.js
+++ script.js
@@ -8,6 +8,38 @@ $(document).ready(readyNow);
 function readyNow() {
 	console.log('JQ');
 	$('#btn-add').on('click', addEmployee);
+	$('#out-employees').on('click', '.btn-delete', deleteEmployee)
+}
+
+function deleteEmployee() {
+	console.log('Clicked on', $(this));
+	const rowToDelete = $(this).closest('tr');
+	console.log('Row to delete', rowToDelete);
+	const employeeToDelete = rowToDelete.data();
+	console.log('Employee to delete', employeeToDelete);
+	rowToDelete.remove();
+
+	// Remove the employee from the array
+	for (let i = 0; i < employeeList.length; i++) {
+		let current = employeeList[i];
+		if (current.employeeID == employeeToDelete.employeeID) {
+			employeeList.splice(i, 1);
+		}
+	}
+
+	// Recalculate the monthly cost
+	updateTotalMonthlyCost()
+}
+
+function updateTotalMonthlyCost() {
+	// Style the footer row if the cost is above limit
+	const monthlyCost = calculateTotalMonthlyCost();
+	if (monthlyCost > MONTHLY_CONST_CAP) {
+		$('tfoot tr').addClass('red');
+	} else {
+		$('tfoot tr').removeClass('red');
+	}
+	$('#out-monthly-cost').text(monthlyCost);
 }
 
 function Employee (first, last, id, title, salary) {
@@ -37,15 +69,9 @@ function addEmployee() {
 
 	// Update the DOM to show employee
 	$('#out-employees').append(createEmployeeHtml(employee));
-
-	// Style the footer row if the cost is above limit
-	const monthlyCost = calculateTotalMonthlyCost();
-	if (monthlyCost > MONTHLY_CONST_CAP) {
-		$('tfoot tr').addClass('red');
-	} else {
-		$('tfoot tr').removeClass('red');
-	}
-	$('#out-monthly-cost').text(monthlyCost);
+	// Add employee data to the last row added to the tbody
+	$('#out-employees tr:last').data(employee);
+	updateTotalMonthlyCost();
 }
 
 function calculateTotalMonthlyCost() {
@@ -64,6 +90,7 @@ function createEmployeeHtml(employee) {
 			<td>${employee.employeeID}</td>
 			<td>${employee.title}</td>
 			<td>${employee.annualSalary}</td>
+			<td><button class="btn-delete">Delete</button></td>
 		</tr>
 	`;
 }
